89bbf76b3965d188b683abf534fd92aba894682b
dub6ix <90335716+dub6ix@users.noreply.github.com>
2023-04-16 15:56:29 -0500

    ugh

--- images/lambda-go-collector/prebuild.sh
+++ images/lambda-go-collector/prebuild.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-# Check for the required number of arguments
 if [ $# -ne 3 ]; then
     echo "Usage: $0 <ENTRY> <OS> <ARCH>"
     exit 1
@@ -11,17 +10,19 @@ OS=$2
 ARCH=$3
 REGION="us-east-1"
 
-# Set AWS region
 export AWS_DEFAULT_REGION="${REGION}"
 
-# Get the AWS Lambda layer ARN
 arn="arn:aws:lambda:${REGION}:901920570463:layer:aws-otel-collector-${ARCH}-ver-0-72-0:1"
-
-# Get the URL to download the Lambda layer
 url="$(aws lambda get-layer-version-by-arn --arn "$arn" --query 'Content.Location' --output text --region "${REGION}")"
 
-# Create the target directory
 mkdir -p "./build/${OS}-${ARCH}"
 
-# Download and extract the Lambda layer in one command
-curl -sL "${url}" | unzip - -d "./build/${OS}-${ARCH}"
+# Download the file to a temporary file
+temp_file=$(mktemp)
+curl -sL "${url}" -o "${temp_file}"
+
+# Extract the downloaded file
+unzip "${temp_file}" -d "./build/${OS}-${ARCH}"
+
+# Remove the temporary file
+rm "${temp_file}"
