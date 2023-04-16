b7959ab5fa70dc84efd439163fcdf306be2ebd2f
dub6ix <90335716+dub6ix@users.noreply.github.com>
2023-04-16 14:30:54 -0500

    cbx

--- images/lambda-go-collector/prebuild.sh
+++ images/lambda-go-collector/prebuild.sh
@@ -5,9 +5,6 @@ OS=$2
 ARCH=$3
 
 export AWS_DEFAULT_REGION="us-east-1"
-export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
-export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
-export AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}"
 
 export arn="arn:aws:lambda:us-east-1:901920570463:layer:aws-otel-collector-$ARCH-ver-0-72-0:1"
 
