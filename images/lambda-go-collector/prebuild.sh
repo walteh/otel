f76553b728e4403e8619721b9737fe70ee623319
dub6ix <90335716+dub6ix@users.noreply.github.com>
2023-04-16 15:51:33 -0500

    gpt

--- images/lambda-go-collector/prebuild.sh
+++ images/lambda-go-collector/prebuild.sh
@@ -1,17 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
+
+# Check for the required number of arguments
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <ENTRY> <OS> <ARCH>"
+    exit 1
+fi
 
 ENTRY=$1
 OS=$2
 ARCH=$3
+REGION="us-east-1"
 
-export AWS_DEFAULT_REGION="us-east-1"
+# Set AWS region
+export AWS_DEFAULT_REGION="${REGION}"
 
-export arn="arn:aws:lambda:us-east-1:901920570463:layer:aws-otel-collector-$ARCH-ver-0-72-0:1"
+# Get the AWS Lambda layer ARN
+arn="arn:aws:lambda:${REGION}:901920570463:layer:aws-otel-collector-${ARCH}-ver-0-72-0:1"
 
-export url="$(aws lambda get-layer-version-by-arn --arn "$arn" --query 'Content.Location' --output text --region 'us-east-1')"
+# Get the URL to download the Lambda layer
+url="$(aws lambda get-layer-version-by-arn --arn "$arn" --query 'Content.Location' --output text --region "${REGION}")"
 
-curl "$url" --output layer.zip
-
+# Create the target directory
 mkdir -p "./build/${OS}-${ARCH}"
 
-unzip -FF layer.zip -d "./build/${OS}-${ARCH}"
+# Download and extract the Lambda layer in one command
+curl -sL "${url}" | unzip - -d "./build/${OS}-${ARCH}"
